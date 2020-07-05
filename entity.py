3452cb65c5f0fac8f24053b269ff9d5bfb87d2b5
christiaan-janssen <christiaanjanssen@outlook.com>
2020-07-05 21:21:05 +0200

    Add fireball spell + casting

--- entity.py
+++ entity.py
@@ -95,6 +95,10 @@ class Entity:
             # Delete the path to free memory
             libtcod.path_delete(my_path)
 
+    def distance(self, x, y):
+        return math.sqrt((x - self.x) ** 2 +  (y - self.y) ** 2)
+
+
     def distance_to(self, other):
         """Calculate the distance between self and other entity"""
         dx = other.x - self.x
